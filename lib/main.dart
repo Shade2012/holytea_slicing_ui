e0ba6df5491a278e8a6d519c65ea98ca1c8e0931
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-10-25 07:25:53 +0700

    style: [CA] styling location page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:holytea_slicing_ui/views/firstpage.dart';
-import 'package:holytea_slicing_ui/views/locationpage.dart';
-import 'package:holytea_slicing_ui/views/privacypage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LocationPage(),
+      home: FirstPage(),
     );
   }
 }
